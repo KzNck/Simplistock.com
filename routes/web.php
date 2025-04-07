d7c9bae87bd36fba45a0e4dbf1beda6be27ce980
KzNck <nicolaadhi1111@gmail.com>
2025-04-07 13:44:40 +0700

    Update web.php
    nambah route ke barang

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::get('/barang', function () {
+    return view('barang.index');
 });
 
 //test
